281abb171203ffa512ad96a4f1084864ff74a0db
omagdiel <omagdiel@gmail.com>
2018-04-03 13:13:20 -0600

    created index page for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,21 @@
 class ProjectsController < ApplicationController
+  before_action :set_project_item, only:[:edit, :update, :show, :destroy]
+
+  def index
+    @projects = Project.all
+  end
+
+  private
+  
+  def project_params
+    params.require(:project).permit(:title,
+                                    :info,
+                                    :main_img,
+                                    :img
+                                  )
+  end
+
+  def set_project_item
+    @project = Project.find(params[:id])
+  end
 end
