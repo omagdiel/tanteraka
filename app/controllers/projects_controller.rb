b687f289186348a587f61df370f350de41487308
omagdiel <omagdiel@gmail.com>
2018-04-03 15:12:19 -0600

    Created update and edit funtion

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,19 +12,31 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
 
-    if @project.save
-      redirect_to @project, notice: 'Your Project was successfully created.'
-    else
-      render :new
+    respond_to do |format|
+      if @project.save
+        format.html{ redirect_to @project, notice: 'Your Project was successfully created.'}
+      else
+        format.html{ render :new }
+      end
     end
   end
 
   def show
 
   end
+  
+  def edit
+
+  end
 
   def update
-
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html{ redirect_to @project, notice: 'Your Project was successfully created.'}
+      else
+        format.html{ render :edit }
+      end
+    end
   end
 
   def destroy
