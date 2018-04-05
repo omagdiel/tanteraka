bb7be433488611d182c7561dd01be998d6dd6174
omagdiel <omagdiel@gmail.com>
2018-04-05 16:28:10 -0600

    Formatted nav bar and fixed some image bugs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,56 @@
 module ApplicationHelper
+
+  def user_nav_items
+    [
+      {
+        url: new_project_path,
+        title: 'New Project'
+      },
+      {
+        url: new_floor_plan_path,
+        title: 'New Floor Plan'
+      }
+    ]
+  end
+
+  def user_nav_helper style, tag_type
+    nav_links = ''
+
+    user_nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
 end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: projects_path,
+        title: 'Projects'
+      },
+      {
+        url: floor_plans_path,
+        title: 'Floor Plans'
+      },
+      {
+        url: pages_contact_path,
+        title: 'Contact'
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,14 @@
   Project.create!(
     title: "House ##{project}",
     info: "This is a nice house with a bunch of features",
-    main_img: "https://via.placeholder.com/350x150",
-    img: "https://via.placeholder.com/350x150"
+    main_img: "https://www.roomsketcher.com/wp-content/uploads/2015/11/RoomSketcher-2-Bedroom-Floor-Plans.jpg"
   )
 end
 
 10.times do |project_image|
   4.times do |project_img|
     ProjectImage.create!(
-      image: "https://www.farmerbrown.com/wp-content/uploads/2017/06/selling-your-home-cedar-shingle-home11.jpg",
+      image: "https://www.roomsketcher.com/wp-content/uploads/2015/11/RoomSketcher-2-Bedroom-Floor-Plans.jpg",
       project_id: "#{project_image + 1}"
     )
   end
@@ -20,8 +19,7 @@ end
   FloorPlan.create!(
     title: "Floor Plan ##{floor_plan}",
     info: "This floor plan is great for families",
-    main_img: "https://via.placeholder.com/350x150",
-    img: "https://via.placeholder.com/350x150"
+    main_img: "https://www.roomsketcher.com/wp-content/uploads/2015/11/RoomSketcher-2-Bedroom-Floor-Plans.jpg"
   )
 end
 
