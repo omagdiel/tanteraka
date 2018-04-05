67f858da39dca16a08df02e8038027347c4bb4c4
omagdiel <omagdiel@gmail.com>
2018-04-05 10:43:29 -0600

    updated seeds with floor plans

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,21 @@ end
     )
   end
 end
+
+10.times do |floor_plan|
+  FloorPlan.create!(
+    title: "Floor Plan ##{floor_plan}",
+    info: "This floor plan is great for families",
+    main_img: "https://via.placeholder.com/350x150",
+    img: "https://via.placeholder.com/350x150"
+  )
+end
+
+10.times do |fp_image|
+  4.times do |fp_img|
+    FloorPlanImage.create!(
+      image: "https://www.roomsketcher.com/wp-content/uploads/2015/11/RoomSketcher-2-Bedroom-Floor-Plans.jpg",
+      floor_plan_id: "#{fp_image + 1}"
+    )
+  end
+end
