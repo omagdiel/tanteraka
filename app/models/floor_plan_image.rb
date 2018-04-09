068f28ddf3efbcb83331791b77d8aac50fe5072d
omagdiel <omagdiel@gmail.com>
2018-04-09 14:28:03 -0600

    Implemented styles for show, home and index pages

--- app/models/floor_plan_image.rb
+++ app/models/floor_plan_image.rb
@@ -1,5 +1,5 @@
 class FloorPlanImage < ApplicationRecord
   belongs_to :floor_plan
 
-    mount_uploader :main_image, FloorPlanImageUploader
+    mount_uploader :image, FloorPlanImageUploader
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,19 @@
+def seed_image(file_name, file_type)
+  File.open(File.join(Rails.root, "/app/assets/images/#{file_name}.#{file_type}"))
+end
+
 10.times do |project|
   Project.create!(
     title: "House ##{project}",
     info: "This is a nice house with a bunch of features",
-    main_img: "https://www.roomsketcher.com/wp-content/uploads/2015/11/RoomSketcher-2-Bedroom-Floor-Plans.jpg"
+    main_img: seed_image('house', 'jpg')
   )
 end
 
 10.times do |project_image|
   4.times do |project_img|
     ProjectImage.create!(
-      image: "https://www.roomsketcher.com/wp-content/uploads/2015/11/RoomSketcher-2-Bedroom-Floor-Plans.jpg",
+      image: seed_image('interior', 'jpeg'),
       project_id: "#{project_image + 1}"
     )
   end
@@ -19,14 +23,14 @@ end
   FloorPlan.create!(
     title: "Floor Plan ##{floor_plan}",
     info: "This floor plan is great for families",
-    main_img: "https://www.roomsketcher.com/wp-content/uploads/2015/11/RoomSketcher-2-Bedroom-Floor-Plans.jpg"
+    main_img: seed_image('wireFrame', 'jpg')
   )
 end
 
 10.times do |fp_image|
   4.times do |fp_img|
     FloorPlanImage.create!(
-      image: "https://www.roomsketcher.com/wp-content/uploads/2015/11/RoomSketcher-2-Bedroom-Floor-Plans.jpg",
+      image: seed_image('floorPlan', 'jpg'),
       floor_plan_id: "#{fp_image + 1}"
     )
   end
