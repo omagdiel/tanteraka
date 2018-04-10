8cea41a92b70ef2e6f9d37f1245941c20f41dbd0
omagdiel <omagdiel@gmail.com>
2018-04-09 18:19:20 -0600

    formated the controller to create emails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,22 @@ class PagesController < ApplicationController
   end
 
   def contact
+    @message = Page.new
+  end
+
+  def create
+    @message = Message.new message_params
+
+    if @message.valid?
+      redirect_to pages_contact_path, notice: "Your message was sent to us and we will get back with you soon."
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def message_params
+    params.require(:message).permit(:name, :email, :body)
   end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,7 @@
 class ContactMailer < ApplicationMailer
+  def contact_me(message)
+    @greeting = "Hi"
+
+    mail to: "omagdiel@gmail.com"
+  end
 end
